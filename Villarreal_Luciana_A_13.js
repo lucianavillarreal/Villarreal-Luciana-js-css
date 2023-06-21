8eadb092a767bd0cf2032950bffa735f796f7014
lucianavillarreal <135833031+lucianavillarreal@users.noreply.github.com>
2023-06-21 17:03:50 -0300

    Villarreal-ejercicio13

---
+++ Villarreal_Luciana_A_13.js
@@ -0,0 +1,19 @@
+for (let i = 1; i <= 17; i++) {
+    if (i % 2 == 0 && i % 7 == 0) {
+      console.log("PlataForma");
+        } 
+    else {
+        if (i % 2 == 0) {
+            console.log("Plata");
+            } 
+         else {
+            if (i % 7 == 0) {
+                console.log("Forma");
+                } 
+            else {
+               console.log(i);
+                }
+            }
+    }    
+
+  }
