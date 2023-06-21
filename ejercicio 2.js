99a6e6562d13d4a316daab9a81953d7bb4fc30b2
lucianavillarreal <135833031+lucianavillarreal@users.noreply.github.com>
2023-06-21 16:50:58 -0300

    Villarreal-ejercicio2

---
+++ ejercicio 2.js
@@ -0,0 +1,17 @@
+const prompt = require('prompt-sync')();
+
+" Pedir por prompt un número y guardarlo en una variable"
+"luego pedir un segundo número y guardarlo en otra variable.2"
+" Crear una tercera variable que sume los valores pedidos y retornarlos por consola."
+
+var n1 = parseInt(prompt("ingresa un numero "));
+var n2 = parseInt(prompt("ingresa otro numero "));
+result = n1 + n2;
+console.log("el resultado de la suma es : " + result);
+
+
+
+
+
+
+
